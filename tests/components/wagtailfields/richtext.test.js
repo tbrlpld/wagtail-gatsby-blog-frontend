9d9c383e053cb79164209c719cfdab175e526365
Tibor Leupold <tibor@lpld.io>
2020-07-01 16:52:46 -1000

    Add meaningful tests for richtext helper function

--- tests/components/wagtailfields/richtext.test.js
+++ tests/components/wagtailfields/richtext.test.js
@@ -4,7 +4,19 @@ import { stripWrappingDivFromRawRichText } from '../../../src/components/wagtail
 
 describe('Helper function to strip wrapping div from raw richtext', () => {
   it('Returns original string if not wrapped by div', () => {
-    const stripped = stripWrappingDivFromRawRichText('Any string')
-    expect(stripped).toEqual('Any string')
+    const notStripped = stripWrappingDivFromRawRichText('Any string')
+    expect(notStripped).toEqual('Any string')
+  })
+
+  it('Does not remove warpping div without the `rich-text` class', () => {
+    const rawRichtext = '<div><p>Some text in the rawtext field.</p></div>'
+    const notStripped = stripWrappingDivFromRawRichText(rawRichtext)
+    expect(notStripped).toEqual(rawRichtext)
+  })
+
+  it('Removes wrapping div', () => {
+    const rawRichtext = '<div class="rich-text"><p>Some text in the rawtext field.</p></div>'
+    const stripped = stripWrappingDivFromRawRichText(rawRichtext)
+    expect(stripped).toEqual('<p>Some text in the rawtext field.</p>')
   })
 })
