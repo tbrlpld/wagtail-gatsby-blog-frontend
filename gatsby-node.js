6515ecac5f204900e485f9fd725c2f165f62a237
Tibor Leupold <tibor@lpld.io>
2020-07-16 14:27:53 -1000

    Fix createPages to really return a promise
    I actually am not sure why it worked before. But once I removed the async statement it
    was not working any more. Also, apparently createPages needs to return a promise to
    work properly. I looked into how `createWagtailPages` is defined and used that approach.
    Basically it is not just chaining actions based on the promise that is returned by the
    `graphql()` function.

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,21 +8,8 @@
 const path = require('path')
 const { createWagtailPages } = require('gatsby-source-wagtail/pages.js')
 
-exports.createPages = async ({ graphql, actions }) => {
-  // Automatically create pages from Wagtail pages
-  createWagtailPages({
-    'home.HomePage': 'templates/home.js',
-    'blog.BlogPage': 'templates/blogpage.js',
-    'blog.BlogIndexPage': 'templates/blogindexpage.jsx',
-    'blog.BlogCategory': 'templates/blogcategory.jsx'
-  }, graphql, actions, [])
-
-  /*
-  Additional pages that do not have a corresponding page in Wagtail but that
-  are still created automatically from data.
-  */
-  const { createPage } = actions
-  const result = await graphql(`
+const createTagPages = (graphql, actions) => {
+  return graphql(`
     query {
       wagtail {
         tags {
@@ -32,16 +19,37 @@ exports.createPages = async ({ graphql, actions }) => {
         }
       }
     }
-  `)
+  `).then(
+    res => {
+      const { createPage } = actions
+      const tags = res.data.wagtail.tags
+      tags.forEach(tag => {
+        createPage({
+          path: `tags/${tag.slug}`,
+          component: path.resolve('./src/templates/tagindexpage.jsx'),
+          context: {
+            tagId: tag.id
+          }
+        })
+      })
+    }
+  )
+}
 
-  const tags = result.data.wagtail.tags
-  tags.forEach(tag => {
-    createPage({
-      path: `tags/${tag.slug}`,
-      component: path.resolve('./src/templates/tagindexpage.jsx'),
-      context: {
-        tagId: tag.id
-      }
-    })
-  })
+exports.createPages = ({ graphql, actions }) => {
+  // Automatically create pages from Wagtail pages
+  return createWagtailPages({
+    'home.HomePage': 'templates/home.js',
+    'blog.BlogPage': 'templates/blogpage.js',
+    'blog.BlogIndexPage': 'templates/blogindexpage.jsx',
+    'blog.BlogCategory': 'templates/blogcategory.jsx'
+  }, graphql, actions, []).then(
+    /*
+    Additional pages that do not have a corresponding page in Wagtail but that
+    are still created automatically from data. Each additional function should be in another `then()`
+    call. This is basically chaining the Promises and makes sure that `createPages` is returning a
+    final promise.
+    */
+    createTagPages(graphql, actions)
+  )
 }
