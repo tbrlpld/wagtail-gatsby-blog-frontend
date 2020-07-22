e74e4f441649a7663cb99aac2d2506a7958e0663
Tibor Leupold <tibor@lpld.io>
2020-07-21 21:46:33 -1000

    Add first example of richtext field that iterates over element
    Currently the iteration is pretty useless. It actually only leads to the elements being nested
    additionally. But, it kind of points toward how the elements might be turned into a
    React component.

--- src/components/wagtailfields/richtext.js
+++ src/components/wagtailfields/richtext.js
@@ -1,17 +1,44 @@
 import React from 'react'
+import { Link } from 'gatsby'
+import cheerio from 'cheerio'
 
-export function stripWrappingDivFromRawRichText (rawRichText) {
-  if (rawRichText.startsWith('<div class="rich-text">')) {
-    return rawRichText.slice(23, -6)
-  } else {
-    return rawRichText
-  }
-}
+// export function stripWrappingDivFromRawRichText (rawRichText) {
+//   if (rawRichText.startsWith('<div class="rich-text">')) {
+//     return rawRichText.slice(23, -6)
+//   } else {
+//     return rawRichText
+//   }
+// }
 
 export default function RichTextField (props) {
   const rawRichText = props.rawRichText
-  const cleanedRichText = stripWrappingDivFromRawRichText(rawRichText)
+
+  const richTextBlocks = []
+
+  const $ = cheerio.load(rawRichText)
+  const rawRichTextBlocks = $('body').children()
+  console.log(rawRichTextBlocks)
+
+  rawRichTextBlocks.each((index, block) => {
+    console.log(block)
+    richTextBlocks.push(
+      <div key={index} dangerouslySetInnerHTML={{ __html: $.html(block) }} />
+    )
+  })
+
+  // const links = $('a[linktype=page]').toArray()
+  // console.log(links)
+  // links.forEach((link) => {
+  //   link.name = 'Link'
+  //   link.attribs.to = '/'
+  // })
+  // console.log(links)
+
+  // const newHTML = $('body').html()
+
   return (
-    <section dangerouslySetInnerHTML={{ __html: cleanedRichText }} />
+    <>
+      {richTextBlocks}
+    </>
   )
 }
