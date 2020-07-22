0a4b0fd4e742f521e19ece2f46115f65eb02a5d7
Tibor Leupold <tibor@lpld.io>
2020-07-21 19:51:18 -1000

    Update header test

--- tests/components/header.test.js
+++ tests/components/header.test.js
@@ -1,12 +1,32 @@
+
 import React from 'react'
+import { useStaticQuery } from 'gatsby'
+
 import renderer from 'react-test-renderer'
 
 import Header from '../../src/components/header.js'
 
 describe('Header', () => {
+  useStaticQuery.mockImplementationOnce(() => (
+    {
+      wagtail: {
+        pages: [{ 
+          id: 1,
+          showInMenus: true,
+          title: 'Testpage',
+          url: 'http://localhost/testpage'
+        }]
+      },
+      site: {
+        siteMetadata: {
+          title: 'Site Title'
+        }
+      }
+    }
+  ))
   it('renders correctly', () => {
     const tree = renderer
-      .create(<Header siteTitle='Default Stater' />)
+      .create(<Header />)
       .toJSON()
     expect(tree).toMatchSnapshot()
   })
