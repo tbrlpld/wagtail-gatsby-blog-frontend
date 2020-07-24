6f7a7574603164fdf616af23a7bab3913cd1dc0b
Tibor Leupold <tibor@lpld.io>
2020-07-23 18:39:41 -1000

    Factor out document link from wagtail link

---
+++ src/components/wagtailfields/documentlink.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useStaticQuery, graphql, Link } from 'gatsby'
+
+export default function DocumentLink ({ documentId, children }) {
+  const data = useStaticQuery(graphql`
+    query {
+      wagtail {
+        documents {
+          id
+          file
+        }
+      }
+      sitePlugin(name: {eq: "gatsby-source-wagtail"}) {
+        pluginOptions {
+          url
+        }
+      }
+    }
+  `)
+  const docs = data.wagtail.documents
+  const doc = docs.filter((doc) => doc.id === documentId).shift()
+  if (doc) {
+    const graphQLEndpointURL = new URL(data.sitePlugin.pluginOptions.url)
+    const backendURL = new URL(graphQLEndpointURL.origin)
+    const fileURL = new URL('media/' + doc.file, backendURL)
+    return <a href={fileURL.href} target='_blank'>{children}</a>
+  } else {
+    return null
+  }
+}
--- src/components/wagtailfields/wagtaillink.jsx
+++ src/components/wagtailfields/wagtaillink.jsx
@@ -1,41 +1,18 @@
 import React from 'react'
-import { Link, useStaticQuery, graphql } from 'gatsby'
 
 import PageLink from './pagelink'
+import DocumentLink from './documentlink'
 
 export default function WagtailLink ({ cheerioBlock, children }) {
   const block = cheerioBlock
-  const data = useStaticQuery(graphql`
-    query {
-      wagtail {
-        documents {
-          id
-          file
-        }
-      }
-      sitePlugin(name: {eq: "gatsby-source-wagtail"}) {
-        pluginOptions {
-          url
-        }
-      }
-    }
-  `)
+
   if (block.attribs.id && block.attribs.linktype === 'page') {
     return <PageLink pageId={block.attribs.id}>{children}</PageLink>
   } else if (block.attribs.id && block.attribs.linktype === 'document') {
-    const docs = data.wagtail.documents
-    const doc = docs.filter((doc) => doc.id === block.attribs.id).shift()
-    if (doc) {
-      const graphQLEndpointURL = new URL(data.sitePlugin.pluginOptions.url)
-      const backendURL = new URL(graphQLEndpointURL.origin)
-      const fileURL = new URL('media/' + doc.file, backendURL)
-      return <a href={fileURL.href}>{children}</a>
-    } else {
-      return null
-    }
+    return <DocumentLink documentId={block.attribs.id}>{children}</DocumentLink>
+  } else {
+    return (
+      <a>{children}</a>
+    )
   }
-
-  return (
-    <a>{children}</a>
-  )
 }
