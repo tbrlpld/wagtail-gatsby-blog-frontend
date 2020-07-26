da3d98c820a14b5265322712a68c45974cea03bb
Tibor Leupold <tibor@lpld.io>
2020-07-25 17:49:32 -1000

    Inline StreamFieldData fragment
    This is still creating some sort of issue.

--- src/templates/blogpage.js
+++ src/templates/blogpage.js
@@ -77,7 +77,27 @@ query ($slug: String) {
           }
         }
         freeformbody {
-          ...StreamFieldData
+          id
+          field
+          rawValue
+          ... on CharBlock {
+            id
+            value
+          }
+          ... on RichTextBlock {
+            id
+            value
+          }
+          ... on ImageChooserBlock {
+            image {
+              id
+              src
+            }
+          }
+          ... on TextBlock {
+            id
+            value
+          }
         }
         tags {
           id
