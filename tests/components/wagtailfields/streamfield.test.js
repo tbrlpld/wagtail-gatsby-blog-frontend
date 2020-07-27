d0f0b1ff544ab191183db252f0358171150ede90
Tibor Leupold <tibor@lpld.io>
2020-07-26 21:25:16 -1000

    Actually fill the test with the logic I need

--- tests/components/wagtailfields/streamfield.test.js
+++ tests/components/wagtailfields/streamfield.test.js
@@ -3,7 +3,8 @@ import { extractSrcFromEmbedIframe } from '../../../src/components/wagtailfields
 
 describe('StreamField Component Helper Function', () => {
   it('Extracts src from embed iframe', () => {
-    const result = extractSrcFromEmbedIframe('html')
-    expect(result).toBe(null)
+    const iframehtml = '<div>\n    <iframe width=\"480\" height=\"270\" src=\"https://www.youtube.com/embed/osBmpCfuzmQ?feature=oembed\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>\n</div>\n'
+    const result = extractSrcFromEmbedIframe(iframehtml)
+    expect(result).toBe('https://www.youtube.com/embed/osBmpCfuzmQ?feature=oembed')
   })
 })
