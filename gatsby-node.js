5bc30bb45d4ffa610efd3758b174e238279a21c3
Tibor Leupold <tibor@lpld.io>
2020-07-31 16:52:12 -1000

    Modify webpack config to ignore CSS order
    Some webpack plugin was throwing errors about css order that do not apply, because
    I am using CSS modules. CSS modules are scoped and the ordering should not matter.

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,7 @@
 // You can delete this file if you're not using it
 const path = require('path')
 const { createWagtailPages } = require('gatsby-source-wagtail/pages.js')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const createTagPages = async (graphql, actions) => {
   return await graphql(`
@@ -55,4 +56,17 @@ exports.createPages = async ({ graphql, actions }) => {
   )
   // Create pages that are not represented in Wagtail
   await createTagPages(graphql, actions)
+}
+
+// Disable CSS order warnings
+// https://stackoverflow.com/questions/63124432/how-do-i-configure-mini-css-extract-plugin-in-gatsby
+exports.onCreateWebpackConfig = ({
+  stage, getConfig, rules, loaders, actions
+}) => {
+  if (stage === 'build-javascript') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(plugin => plugin.constructor.name === 'MiniCssExtractPlugin')
+    miniCssExtractPlugin.options.ignoreOrder = true
+    actions.replaceWebpackConfig(config)
+  }
 }
