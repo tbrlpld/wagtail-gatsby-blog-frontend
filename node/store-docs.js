1cfabfb7f246c34b050485e7f8a87cf3f2cf4c57
Tibor Leupold <tibor@lpld.io>
2020-08-14 12:26:30 -1000

    Use async await on store docs module

--- node/store-docs.js
+++ node/store-docs.js
@@ -1,28 +1,28 @@
 const fsPromises = require('fs').promises
 const path = require('path')
 
-const ensureDirectoryExistence = (dir) => {
+const ensureDirectoryExistence = async (dir) => {
   console.log('Checking for directory existence: ' + dir)
   const testFilePath = path.resolve(dir, 'testfile.txt')
 
-  return fsPromises.mkdir(dir, { recursive: true })
-    .then(() => console.log('Directory exists or created.'))
-    .then(() => {
-      console.log('Now that the directory exists, I can do something with it.')
-      console.log('Checking for the testfile.')
-      return fsPromises.stat(testFilePath)
-    })
-    .then((stats) => console.log('The testfile does exist already.'))
-    .catch((err) => {
-      console.log('Testfile does not exist: ' + err)
-      console.log('Creating...')
-      const testFileContent = 'Some nonsense string'
-      return fsPromises.writeFile(testFilePath, testFileContent)
-    })
+  await fsPromises.mkdir(dir, { recursive: true })
+
+  console.log('Directory exists or created.')
+  console.log('Now that the directory exists, I can do something with it.')
+  console.log('Checking for the testfile.')
+
+  try {
+    await fsPromises.stat(testFilePath)
+  } catch (err) {
+    console.log('Testfile does not exist: ' + err)
+    console.log('Creating...')
+    const testFileContent = 'Some nonsense string'
+    await fsPromises.writeFile(testFilePath, testFileContent)
+  }
 }
 
 const storeDocuments = async (dir) => {
-  const dirPromise = ensureDirectoryExistence(dir)
+  await ensureDirectoryExistence(dir)
 }
 
 module.exports = storeDocuments
