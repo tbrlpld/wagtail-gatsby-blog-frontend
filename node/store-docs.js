273fa72f4e812d702bd68f270a1a16f51e734ef5
Tibor Leupold <tibor@lpld.io>
2020-08-16 19:39:24 -1000

    Remove outdated function
    This function was only my first attempt to interact with the filesystem with JS.
    It is not needed anymore, because it was all just wrapping basically on single
    function call.

--- node/store-docs.js
+++ node/store-docs.js
@@ -48,26 +48,6 @@ const removeExistingFilesFromArray = async (fileArray) => {
 }
 exports.removeExistingFilesFromArray = removeExistingFilesFromArray
 
-const ensureDirectoryExistence = async (dir) => {
-  console.log('Checking for directory existence: ' + dir)
-  const testFilePath = path.resolve(dir, 'testfile.txt')
-
-  await fsPromises.mkdir(dir, { recursive: true })
-
-  console.log('Directory exists or created.')
-  console.log('Now that the directory exists, I can do something with it.')
-  console.log('Checking for the testfile.')
-
-  try {
-    await fsPromises.stat(testFilePath)
-  } catch (err) {
-    console.log('Testfile does not exist: ' + err)
-    console.log('Creating...')
-    const testFileContent = 'Some nonsense string'
-    await fsPromises.writeFile(testFilePath, testFileContent)
-  }
-}
-
 /**
  * Download file from source and store to path.
  *
@@ -112,7 +92,7 @@ const storeWagtailDocuments = async (storageDir, graphql) => {
       }
     }
   `)
-  const ensuredStorageDirExistence = ensureDirectoryExistence(storageDir)
+  const ensuredStorageDirExistence = await fsPromises.mkdir(storageDir, { recursive: true })
   const results = await Promise.all([documentsResponse, ensuredStorageDirExistence])
 
   const wagtailDocuments = results[0].data.wagtail.documents
